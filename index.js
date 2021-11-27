4e7439282135d68f0f35d5dbe7316b19b663fe96
Ionut Alexandru Tudor-Pricop <81566145+BLITzStudio@users.noreply.github.com>
2021-11-27 19:58:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 443;
+const port = 8080;
 
 app.listen(port, () => {
   console.log("The server is listening on port 443");
